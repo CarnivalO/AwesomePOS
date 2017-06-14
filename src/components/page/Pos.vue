833ce113510d0bc99379a441ab5d8a8fa1480665
CarnivalO <xmwjn15@gmail.com>
2017-06-14 17:32:51 +0800

    aa

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -6,13 +6,13 @@
           <el-tab-pane label="点餐">
             <el-table :data="tableData" border style="width:100%">
               <el-table-column prop="goodsName" label="商品名称">
-
+  
               </el-table-column>
               <el-table-column prop="count" label="量" width="60">
-
+  
               </el-table-column>
               <el-table-column prop="price" label="金额" width="70">
-
+  
               </el-table-column>
               <el-table-column label="操作" width="100" fixed="right">
                 <template scope="scope">
@@ -33,9 +33,9 @@
           <el-tab-pane label="外卖">
             外卖
           </el-tab-pane>
-
+  
         </el-tabs>
-
+  
       </el-col>
       <!--商品展示-->
       <el-col :span="17">
@@ -43,21 +43,23 @@
           <div class="title">常用商品</div>
           <div class="often-goods-list">
             <ul>
-              <li v-for="goods in oftenGoods">
+              <li v-for="goods in oftenGoods" @click=addOrderList()>
                 <span>{{goods.goodsName}}</span>
                 <span class="o-price">{{goods.price}}</span>
               </li>
             </ul>
           </div>
         </div>
-
+  
         <div class="goods-type">
           <el-tabs>
             <el-tab-pane label="汉堡">
               <div>
                 <ul class='cookList'>
                   <li v-for="goods in type0Goods">
-                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodImg">
+                      <img :src="goods.goodsImg" width="100%">
+                    </span>
                     <span class="foodName">{{goods.goodsName}}</span>
                     <span class="foodPrice">￥{{goods.price}}元</span>
                   </li>
@@ -65,169 +67,166 @@
               </div>
             </el-tab-pane>
             <el-tab-pane label="小食">
-             <ul class='cookList'>
-                  <li v-for="goods in type1Goods">
-                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
-                    <span class="foodName">{{goods.goodsName}}</span>
-                    <span class="foodPrice">￥{{goods.price}}元</span>
-                  </li>
-                </ul>
+              <ul class='cookList'>
+                <li v-for="goods in type1Goods">
+                  <span class="foodImg">
+                    <img :src="goods.goodsImg" width="100%">
+                  </span>
+                  <span class="foodName">{{goods.goodsName}}</span>
+                  <span class="foodPrice">￥{{goods.price}}元</span>
+                </li>
+              </ul>
             </el-tab-pane>
             <el-tab-pane label="饮料">
-             <ul class='cookList'>
-                  <li v-for="goods in type2Goods">
-                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
-                    <span class="foodName">{{goods.goodsName}}</span>
-                    <span class="foodPrice">￥{{goods.price}}元</span>
-                  </li>
-                </ul>
+              <ul class='cookList'>
+                <li v-for="goods in type2Goods">
+                  <span class="foodImg">
+                    <img :src="goods.goodsImg" width="100%">
+                  </span>
+                  <span class="foodName">{{goods.goodsName}}</span>
+                  <span class="foodPrice">￥{{goods.price}}元</span>
+                </li>
+              </ul>
             </el-tab-pane>
             <el-tab-pane label="套餐">
-             <ul class='cookList'>
-                  <li v-for="goods in type3Goods">
-                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
-                    <span class="foodName">{{goods.goodsName}}</span>
-                    <span class="foodPrice">￥{{goods.price}}元</span>
-                  </li>
-                </ul>
+              <ul class='cookList'>
+                <li v-for="goods in type3Goods">
+                  <span class="foodImg">
+                    <img :src="goods.goodsImg" width="100%">
+                  </span>
+                  <span class="foodName">{{goods.goodsName}}</span>
+                  <span class="foodPrice">￥{{goods.price}}元</span>
+                </li>
+              </ul>
             </el-tab-pane>
           </el-tabs>
         </div>
-
-
+  
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
-
-  export default {
-    name: 'pos',
-    created () {
-      axios.get('http://jspang.com/DemoApi/oftenGoods.php')
-        .then(response => {
-          console.log(response)
-          this.oftenGoods = response.data
-        })
-        .catch(error => {
-          console.log(error)
-          alert('网络错误，不能访问')
-        })
-      axios.get('http://jspang.com/DemoApi/typeGoods.php')
-        .then(response => {
-          console.log(response)
-          this.type0Goods = response.data[0]
-          this.type1Goods = response.data[1]
-          this.type2Goods = response.data[2]
-          this.type3Goods = response.data[3]
-        })
-        .catch(error => {
-          console.log(error)
-          alert('网络错误，不能访问')
-        })
-    },
-    data () {
-      return {
-        tableData: [{
-          goodsName: '可口可乐',
-          price: 8,
-          count: 1
-        }, {
-          goodsName: '香辣鸡腿堡',
-          price: 15,
-          count: 1
-        }, {
-          goodsName: '爱心薯条',
-          price: 8,
-          count: 1
-        }, {
-          goodsName: '甜筒',
-          price: 8,
-          count: 1
-        }],
-        oftenGoods: [],
-        type0Goods: [],
-        type1Goods: [],
-        type2Goods: [],
-        type3Goods: []
+import axios from 'axios'
+export default {
+  name: 'pos',
+  created() {
+    axios.get('http://jspang.com/DemoApi/oftenGoods.php')
+      .then(response => {
+        this.oftenGoods = response.data
+      })
+      .catch(error => {
+        console.log(error)
+        alert('网络错误，不能访问')
+      })
+    axios.get('http://jspang.com/DemoApi/typeGoods.php')
+      .then(response => {
+        console.log(response)
+        this.type0Goods = response.data[0]
+        this.type1Goods = response.data[1]
+        this.type2Goods = response.data[2]
+        this.type3Goods = response.data[3]
+      })
+      .catch(error => {
+        console.log(error)
+        alert('网络错误，不能访问')
+      })
+  },
+  data() {
+    return {
+      tableData: [],
+      oftenGoods: [],
+      type0Goods: [],
+      type1Goods: [],
+      type2Goods: [],
+      type3Goods: []
+    }
+  },
+  mounted: function () {
+    var orderHeight = document.body.clientHeight
+    document.getElementById('order-list').style.height = orderHeight + 'px'
+  },
+  methods: {
+    addOrderList(goods) {
+      let isHave = false;
+      for (let i = 0; i < this.tableData.length; i++) {
+        if (this.tableData[i].goodsId == goods.goodsId) {
+          isHave = true;
+        }
       }
-    },
-    mounted: function () {
-      var orderHeight = document.body.clientHeight
-      document.getElementById('order-list').style.height = orderHeight + 'px'
+      if(isHave){
+
+      }
     }
   }
-
 </script>
 
 <style scoped>
-  .pos-order {
-    background-color: #F9FAFC;
-    border-right: 1px solid #C0CCDA;
-  }
+.pos-order {
+  background-color: #F9FAFC;
+  border-right: 1px solid #C0CCDA;
+}
 
-  .div-btn {
-    margin-top: 10px;
-  }
+.div-btn {
+  margin-top: 10px;
+}
 
-  .title {
-    height: 20px;
-    border-bottom: 1px solid #D3DCE6;
-    background-color: #F9FAFC;
-    padding: 10px;
-  }
+.title {
+  height: 20px;
+  border-bottom: 1px solid #D3DCE6;
+  background-color: #F9FAFC;
+  padding: 10px;
+}
 
-  .often-goods-list ul li {
-    list-style: none;
-    float: left;
-    border: 1px solid #E5E9F2;
-    padding: 10px;
-    margin: 5px;
-    background-color: #fff;
-  }
+.often-goods-list ul li {
+  list-style: none;
+  float: left;
+  border: 1px solid #E5E9F2;
+  padding: 10px;
+  margin: 5px;
+  background-color: #fff;
+}
 
-  .o-price {
-    color: #58B7FF;
-  }
+.o-price {
+  color: #58B7FF;
+}
 
-  .goods-type {
-    clear: both;
-  }
+.goods-type {
+  clear: both;
+}
 
-  .cookList li {
-    list-style: none;
-    width: 23%;
-    border: 1px solid #E5E9F2;
-    height: auto;
-    overflow: hidden;
-    background-color: #fff;
-    padding: 2px;
-    float: left;
-    margin: 2px;
-  }
+.cookList li {
+  list-style: none;
+  width: 23%;
+  border: 1px solid #E5E9F2;
+  height: auto;
+  overflow: hidden;
+  background-color: #fff;
+  padding: 2px;
+  float: left;
+  margin: 2px;
+}
 
-  .cookList li span {
+.cookList li span {
 
-    display: block;
-    float: left;
-  }
+  display: block;
+  float: left;
+}
 
-  .foodImg {
-    width: 40%;
-  }
+.foodImg {
+  width: 40%;
+}
 
-  .foodName {
-    font-size: 18px;
-    padding-left: 10px;
-    color: brown;
-  }
+.foodName {
+  font-size: 16px;
+  padding-left: 10px;
+  color: brown;
+}
 
-  .foodPrice {
-    font-size: 16px;
-    padding-left: 10px;
-    padding-top: 10px;
-  }
-
+.foodPrice {
+  font-size: 16px;
+  padding-left: 10px;
+  padding-top: 10px;
+}
 </style>
